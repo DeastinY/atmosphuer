a5ebc59c9f25fb3afc113b3519a484ca1898cde1
Richard Polzin <polzin.richard@gmail.com>
2015-10-18 12:02:29 +0200

    can now exit and write json sample configs

--- test.py
+++ test.py
@@ -4,9 +4,47 @@ from phue import Bridge
 import logging
 import json
 
+def print_options():
+    print("")
+    print("")
+    print("")
+    print("Here are the available options:")
+    print("end\tEnd Programm")
+    print("cs\tCreate Settings")
+    pass
+
+def write_sample_json_file():
+    print("writing json samples")
+    #{"name": "rain", "brightness": "254", "hue": "15000", "saturation": "120", "audio": "rain.mp3" }
+    json_scenes = {
+        "name": "rain",
+        "brightness": "254",
+        "hue": "15000",
+        "saturation": "120",
+        "audio": "rain.mp3",
+    }
+
+    #{"bridge_ip": "192.168.0.10", "shadowrun_lights": "Arbeitszimmer"}
+    json_settings = {
+        "bridge_ip": "192.168.0.10",
+        "shadowrun_lights": "Arbeitszimmer",
+    }
+
+    #write to file
+    f = open("scenes.json","w")
+    f.write(json.dumps(json_scenes))
+    f.close()
+
+    f = open("settings.json","w")
+    f.write(json.dumps(json_settings))
+    f.close()
+
+    print("files written")
+
 #set up a basic config for the logger
 logging.basicConfig()
 
+# (later from JSON)
 b = Bridge('192.168.0.10')
 
 # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
@@ -19,35 +57,26 @@ lights = b.get_light_objects('name')
 for light in lights:
     print ("\t"+light)
 
-# Set up relevant SR lights (later from JSON)
-sr = [lights["Arbeitszimmer"]]#,lights[]"Esszimmer"]]
+    # Set up relevant SR lights (later from JSON)
+    sr = [lights["Arbeitszimmer"]]#,lights[]"Esszimmer"]]
 
-# Activate lamp
-for light in sr:
-    light.on = True
-    light.brightness = 254
+    # Activate lamp
+    for light in sr:
+        light.on = True
+        light.brightness = 254
+
+
+# Ask user for input until he closes the program
+input = ""
+while input != "end":
+    print_options()
+    input = raw_input("Enter command:")
+    if input == "cs":
+        write_sample_json_file()
 
 # Deactivate lamp
 for light in sr:
     light.on = False
-
-# Get the bridge state (This returns the full dictionary that you can explore) (b.get_api())
-
-# Prints if light 1 is on or not b.get_light(1, 'on')
-
-# Set brightness of lamp 1 to max b.set_light(1, 'bri', 254)
-
-# Set brightness of lamp 2 to 50% b.set_light(2, 'bri', 127)
-
-# Turn lamp 2 on b.set_light(2,'on', True)
-
-# You can also control multiple lamps by sending a list as lamp_id b.set_light( [1,2], 'on', True)
-
-# Get the name of a lamp b.get_light(1, 'name')
-
-# You can also use light names instead of the id b.get_light('Kitchen') b.set_light('Kitchen', 'bri', 254)
-
-# Also works with lists b.set_light(['Bathroom', 'Garage'], 'on', False)
 
 # The set_light method can also take a dictionary as the second argument to do more fancy stuff
 # This will turn light 1 on with a transition time of 30 seconds
